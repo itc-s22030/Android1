f35a4cade9f53db2d79a5638b608a1f41bf54b4d
s22030 <s22030@std.it-college.ac.jp>
2023-07-27 14:40:22 +0900

    7.1 終わり

--- IntentSample/app/src/main/java/jp/ac/it_college/std/s22030/intentsample/MainActivity.kt
+++ IntentSample/app/src/main/java/jp/ac/it_college/std/s22030/intentsample/MainActivity.kt
@@ -2,10 +2,28 @@ package jp.ac.it_college.std.s22030.intentsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import jp.ac.it_college.std.s22030.intentsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.lvMenu.apply {
+            // アダプタとデータをセット
+            adapter = MenuListAdapter(menuList)
+
+            // RecyclerView の表示に関する設定
+            LinearLayoutManager(this@MainActivity).let {
+                // 直線的に並べる
+                layoutManager = it
+                // 区切り線を並べる方向に合わせて表示する
+                addItemDecoration(DividerItemDecoration(this@MainActivity, it.orientation))
+            }
+        }
     }
 }
