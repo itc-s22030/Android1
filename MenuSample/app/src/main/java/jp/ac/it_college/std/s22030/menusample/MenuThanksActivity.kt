8d3a087713e717259a0305ed451bab5686973cd7
s22030 <s22030@std.it-college.ac.jp>
2023-08-09 14:50:39 +0900

    8.3までできた

--- MenuSample/app/src/main/java/jp/ac/it_college/std/s22030/menusample/MenuThanksActivity.kt
+++ MenuSample/app/src/main/java/jp/ac/it_college/std/s22030/menusample/MenuThanksActivity.kt
@@ -2,6 +2,7 @@ package jp.ac.it_college.std.s22030.menusample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import jp.ac.it_college.std.s22030.menusample.databinding.ActivityMenuThanksBinding
 
 class MenuThanksActivity : AppCompatActivity() {
@@ -21,5 +22,18 @@ class MenuThanksActivity : AppCompatActivity() {
         binding.btThxBack.setOnClickListener {
             finish()
         }
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val result = when(item.itemId){
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+        return result
     }
 }
